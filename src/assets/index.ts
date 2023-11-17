0e0cbc0c2777fd39842db084ccc86ec80754e84c
Callum Hauber <callumhauber@gmail.com>
2023-11-16 23:35:29 -0500

    add background & css

---
+++ src/assets/index.ts
@@ -0,0 +1,1 @@
+export { default as background } from './images/icons/sleepy.png';
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,32 @@
+import { Global, css } from '@emotion/react';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import { background } from './assets';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Global
+        styles={css`
+          html {
+            width: 100%;
+            margin: auto;
+          }
+
+          body {
+            width: 100%;
+            margin: auto;
+            background-image: url(${background});
+            background-size: auto;
+            background-repeat: repeat;
+          }
+        `}
+      />
     </BrowserRouter>
   </StrictMode>
 );
