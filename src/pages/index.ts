9deead96ea7d611ad360e8e22726d54939b9241e
Callum Hauber <callumhauber@gmail.com>
2024-02-15 22:26:58 -0500

    rename main to home

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
-export * from './main/main';
+export * from './home/home';
 export * from './about/about';
 export * from './commissions/commissions';
 export * from './projects/projects';
 export * from './fwends/fwends';
-
