3a768385a734aaa5793e84720106a344070201f6
Callum Hauber <callumhauber@gmail.com>
2024-02-15 23:45:00 -0500

    add breadcrumbs

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,69 @@
 import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
 
 import { background } from './assets';
 import { About, Home, Commissions, Projects, Fwends } from './pages';
 
+const StyledLink = styled(Link)`
+  border: none;
+  text-decoration: none !important;
+  color: #e9b05c;
+
+  &:hover {
+    border: none;
+    text-decoration: none;
+  }
+
+  &:visited {
+    color: #e9b05c;
+    border: none;
+    text-decoration: none;
+  }
+`;
+
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/about', element: <About /> },
-  { path: '/commissions', element: <Commissions /> },
-  { path: '/projects', element: <Projects /> },
-  { path: '/fwends', element: <Fwends /> }
+  {
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />,
+        handle: {
+          crumb: () => 'about'
+        }
+      },
+      {
+        path: '/commissions',
+        element: <Commissions />,
+        handle: {
+          crumb: () => 'commissions'
+        }
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+        handle: {
+          crumb: () => 'projects'
+        }
+      },
+      {
+        path: '/fwends',
+        element: <Fwends />,
+        handle: {
+          crumb: () => 'fwends'
+        }
+      }
+    ],
+    handle: {
+      crumb: () => <StyledLink to="/">home</StyledLink>
+    }
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
