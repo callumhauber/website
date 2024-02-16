75d046ab0ffca40da047b656103120ba01bc638d
Callum Hauber <callumhauber@gmail.com>
2024-02-15 22:50:10 -0500

    rename button to sweep button

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/Breadcrumb/Breadcrumb';
 export * from './lib/FlexCenter/FlexCenter';
 export * from './lib/NavBox/NavBox';
-export * from './lib/button/SweepButton';
+export * from './lib/SweepButton/SweepButton';
 export * from './lib/ui';
