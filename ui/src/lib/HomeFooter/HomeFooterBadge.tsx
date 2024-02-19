cc41e3cd36dfafe6f108d1d20bc4f73198005cca
Callum Hauber <callumhauber@gmail.com>
2024-02-19 18:35:13 -0500

    fix spacing and make signature not a link

--- ui/src/lib/HomeFooter/HomeFooterBadge.tsx
+++ ui/src/lib/HomeFooter/HomeFooterBadge.tsx
@@ -1,14 +1,17 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 /* eslint-disable-next-line */
 export interface HomeFooterProps {
   children: React.ReactNode;
-  href: string;
+  href?: string;
+  rel?: string;
 }
 
-const StyledHomeFooterCard = styled.a`
+const StyledBadgeLink = styled.a`
   border: none;
   text-decoration: none;
+  padding: 0px 2px;
 
   &:hover,
   &:visited {
@@ -17,12 +20,25 @@ const StyledHomeFooterCard = styled.a`
   }
 `;
 
-export function HomeFooterBadge({ children, href }: HomeFooterProps) {
-  return (
-    <StyledHomeFooterCard target="_blank" href={href}>
-      {children}
-    </StyledHomeFooterCard>
-  );
+export function HomeFooterBadge({ children, href, rel = 'noreferrer' }: HomeFooterProps) {
+  if (href) {
+    return (
+      // eslint-disable-next-line react/jsx-no-target-blank
+      <StyledBadgeLink target="_blank" rel={rel} href={href}>
+        {children}
+      </StyledBadgeLink>
+    );
+  } else {
+    return (
+      <span
+        css={css`
+          padding: 0px 2px;
+        `}
+      >
+        {children}
+      </span>
+    );
+  }
 }
 
 export default HomeFooterBadge;
