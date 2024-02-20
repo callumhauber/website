d9cc8b0e2a98ea925c741c8e8425c4b806ebf3ef
Callum Hauber <callumhauber@gmail.com>
2024-02-19 19:32:41 -0500

    add different backgrounds for different pages

--- src/pages/fwends/fwends.tsx
+++ src/pages/fwends/fwends.tsx
@@ -1,19 +1,27 @@
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Breadcrumb, PageHeader } from '@website/ui';
+
+import { backgroundHappy } from '../../assets';
 
 /* eslint-disable-next-line */
 export interface FwendsProps {}
 
-const StyledAbout = styled.div`
-  color: pink;
-`;
+const StyledFwends = styled.div``;
 
 export function Fwends(props: FwendsProps) {
   return (
-    <>
+    <StyledFwends>
+      <Global
+        styles={css`
+          body {
+            background-image: url(${backgroundHappy});
+          }
+        `}
+      />
       <Breadcrumb />
       <PageHeader heading="fwends!!" subheading="folks you should know about" />
-    </>
+    </StyledFwends>
   );
 }
 
--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,19 +1,27 @@
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Breadcrumb, PageHeader } from '@website/ui';
+
+import { backgroundXTile } from '../../assets';
 
 /* eslint-disable-next-line */
 export interface ProjectsProps {}
 
-const StyledAbout = styled.div`
-  color: pink;
-`;
+const StyledProjects = styled.div``;
 
 export function Projects(props: ProjectsProps) {
   return (
-    <>
+    <StyledProjects>
+      <Global
+        styles={css`
+          body {
+            background-image: url(${backgroundXTile});
+          }
+        `}
+      />
       <Breadcrumb />
       <PageHeader heading="projects" subheading="where non-art activities live" />
-    </>
+    </StyledProjects>
   );
 }
 
