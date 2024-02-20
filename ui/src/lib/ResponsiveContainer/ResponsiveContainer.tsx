2e328910bce4813ed240e6142b7318ae41f7d9e5
Callum Hauber <callumhauber@gmail.com>
2024-02-19 23:45:23 -0500

    add accordion to about page

---
+++ ui/src/lib/ResponsiveContainer/ResponsiveContainer.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+/* eslint-disable-next-line */
+export interface ResponsiveContainerProps {
+  children: React.ReactNode;
+}
+
+const StyledResponsiveContainer = styled.div`
+  margin: auto;
+  max-width: 60em;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export function ResponsiveContainer({ children }: ResponsiveContainerProps) {
+  return <StyledResponsiveContainer>{children}</StyledResponsiveContainer>;
+}
+
+export default ResponsiveContainer;
