74d9db0c534e2b6b0ffad274a576bccef3da6cb8
Callum Hauber <callumhauber@gmail.com>
2024-02-19 19:32:41 -0500

    add different backgrounds for different pages

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,19 +1,27 @@
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Breadcrumb, PageHeader } from '@website/ui';
+
+import { backgroundFishy2 } from '../../assets';
 
 /* eslint-disable-next-line */
 export interface AboutProps {}
 
-const StyledAbout = styled.div`
-  color: pink;
-`;
+const StyledAbout = styled.div``;
 
 export function About(props: AboutProps) {
   return (
-    <>
+    <StyledAbout>
+      <Global
+        styles={css`
+          body {
+            background-image: url(${backgroundFishy2});
+          }
+        `}
+      />
       <Breadcrumb />
       <PageHeader heading="about" subheading="me, my site, and my silly little hobbies" />
-    </>
+    </StyledAbout>
   );
 }
 
--- src/pages/commissions/commissions.tsx
+++ src/pages/commissions/commissions.tsx
@@ -1,19 +1,27 @@
+import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Breadcrumb, PageHeader } from '@website/ui';
+
+import { backgroundXTile } from '../../assets';
 
 /* eslint-disable-next-line */
 export interface CommissionsProps {}
 
-const StyledAbout = styled.div`
-  color: pink;
-`;
+const StyledCommissions = styled.div``;
 
 export function Commissions(props: CommissionsProps) {
   return (
-    <>
+    <StyledCommissions>
+      <Global
+        styles={css`
+          body {
+            background-image: url(${backgroundXTile});
+          }
+        `}
+      />
       <Breadcrumb />
       <PageHeader heading="commissions" subheading="so, you want to give me money..." />
-    </>
+    </StyledCommissions>
   );
 }
 
