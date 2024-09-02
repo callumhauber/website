be9db1d0487490e57ca51523abf9ba0fc28927b5
Callum Hauber <callumhauber@gmail.com>
2024-09-02 00:11:56 -0400

    bump to nx 19.6.4

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,7 +4,7 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
 
 /**
  * Read environment variables from file.
@@ -25,8 +25,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm exec nx serve website',
-    url: 'http://localhost:4200',
+    command: 'pnpm exec nx run website:preview',
+    url: 'http://localhost:4300',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
   },
