4318fcb04c5693feacc362209dfd719d9f15bc22
Callum Hauber <callumhauber@gmail.com>
2025-01-07 22:25:58 -0500

    don't add hash to assets

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   },
   cacheDir: './node_modules/.vite/website',
 
